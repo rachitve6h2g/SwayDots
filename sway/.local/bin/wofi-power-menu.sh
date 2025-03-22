2d076e0073ae85680d8f0b49d90fd8d7fc53c099
Rachit Kumar Verma <rachitverma1122@gmail.com>
2025-03-22 23:22:12 +0530

    Wofi native window enabled

--- sway/.local/bin/wofi-power-menu.sh
+++ sway/.local/bin/wofi-power-menu.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-op=$( echo -e "  Poweroff\n  Reboot\n  Suspend\n  Lock\n  Logout" | wofi -i -n --dmenu --width 300 --height 200 | awk '{print tolower($2)}' )
+op=$( echo -e "  Poweroff\n  Reboot\n  Suspend\n  Lock\n  Logout" | wofi -i --dmenu --width 300 --height 300 -x 1590  -y 300 | awk '{print tolower($2)}' )
 
 case $op in 
   poweroff)
